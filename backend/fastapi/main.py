fa6c35fad8b4cc6110c213476b4a665f109d7285
Raunak Lanjewar <raunaklanjewar42@gmail.com>
2024-08-31 17:35:15 +0530

    File creation logic and dicts creation.

--- backend/fastapi/main.py
+++ backend/fastapi/main.py
@@ -3,6 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from helpers import greet, process_and_merge_datasets, filter_and_summarize, group_and_categorize_by_order_id,create_markings
 import logging
+import os
 
 logger = logging.getLogger(__name__) 
 
@@ -41,10 +42,24 @@ async def upload_files(
     mtrFile: UploadFile = File(...),
     prsFile: UploadFile = File(...)
 ):
+    os.makedirs('processed_data', exist_ok=True)
     merged_df = await process_and_merge_datasets(mtrFile, prsFile)
-    summary = filter_and_summarize(merged_df)
-    grouped = group_and_categorize_by_order_id(merged_df)
-    categorized = create_markings(grouped)
+    
+    blank_transaction_df, blank_transaction_summary_df = filter_and_summarize(merged_df)
+    grouped_df = group_and_categorize_by_order_id(merged_df)
+    categorized_df,categorized_value_counts_df = create_markings(grouped_df)
+    
+    merged_df.to_excel(os.path.join('.','processed_data','Merged_Dataset.xlsx'),index=False)
+    blank_transaction_df.to_excel(os.path.join('.','processed_data','Blank_Order_Id_Transactions.xlsx'), index=False)
+    blank_transaction_summary_df.to_excel(os.path.join('.','processed_data','Blank_Transaction_Summary.xlsx'), index=False)
+    categorized_df.to_excel(os.path.join('.','processed_data','Categorized_Transaction_Record.xlsx'), index=False)
+    categorized_value_counts_df.to_excel(os.path.join('.','processed_data','Transaction_Summary.xlsx'), index=False)
+
+    
+    blank_transaction_records = blank_transaction_df.to_dict(orient='records')
+    blank_transaction_summary_records = blank_transaction_summary_df.to_dict(orient='records')
+    categorized_df_records = categorized_df.to_dict(orient='records')
+    categorized_value_counts_df_records = categorized_value_counts_df.to_dict(orient='records')
     
     
     return {"message": "Files processed and saved successfully."}   
