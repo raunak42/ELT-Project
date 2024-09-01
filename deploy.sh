7b3588846d0fcb92de821afec384f4aa8d2e6aec
Raunak Lanjewar <raunaklanjewar42@gmail.com>
2024-09-01 16:47:06 +0530

    Deployment script modified.

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,12 @@ export PATH=$PATH:/home/ubuntu/.nvm/versions/node/v20.5.1/bin
 
 echo "Deploying backend..."
 cd backend
+if [ ! -d ".venv" ]; then
+    echo "Creating virtual environment..."
+    python3 -m venv .venv
+fi
+
+echo "Activating virtual environment..."
 source .venv/bin/activate
 cd fastapi
 pip install -r requirements.txt
