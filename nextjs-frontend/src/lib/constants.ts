a2169ffc60552bd0b436c3052bbf5c6bdade7fb3
Raunak Lanjewar <raunaklanjewar42@gmail.com>
2024-09-01 17:58:34 +0530

    Minor changes.

--- nextjs-frontend/src/lib/constants.ts
+++ nextjs-frontend/src/lib/constants.ts
@@ -1,4 +1,4 @@
 const BASE_URL_DEV = "http://localhost:8000"
-const BASE_URL_PROD = "https://interface.raunak42.in"
+const BASE_URL_PROD = "https://interfaceapi.raunak42.in"
 
 export const BASE_URL = process.env.NODE_ENV === 'production' ? BASE_URL_PROD : BASE_URL_DEV;
