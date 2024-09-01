5df14e7b2a2a8678756614f3aeef834c171e0be9
Raunak Lanjewar <raunaklanjewar42@gmail.com>
2024-09-01 17:09:05 +0530

    Script changes.

--- deploy.sh
+++ deploy.sh
@@ -7,15 +7,13 @@ echo "Deploying backend..."
 cd backend
 echo "Creating virtual environment..."
 python3 -m venv .venv
+source .venv/bin/activate
 
 echo "Installing python dependencies..."
-
 cd fastapi
 pip install -r requirements.txt
 pip cache purge
-echo "Activating virtual environment..."
 cd ..
-source .venv/bin/activate
 pm2 delete fastapi-server || true
 pm2 start main.py --name fastapi-server --interpreter python3
 
