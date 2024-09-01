18c74610a3099446a2c87e7b68519ff3d896a613
Raunak Lanjewar <raunaklanjewar42@gmail.com>
2024-09-01 16:37:55 +0530

    Contants file created.

---
+++ nextjs-frontend/src/lib/constants.ts
@@ -0,0 +1,4 @@
+const BASE_URL_DEV = "http://localhost:8000"
+const BASE_URL_PROD = "https://interface.raunak42.in"
+
+export const BASE_URL = process.env.NODE_ENV === 'production' ? BASE_URL_PROD : BASE_URL_DEV;
